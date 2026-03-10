57ca23a8d52f2ce1066679984be0a4d4c443a916
Divyanshu0809 <divyanshu0809@gmail.com>
2026-03-10 09:25:47 +0530

    Linked List Based Palindrome Checker

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,26 +1,55 @@
 import java.util.*;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word : ");
-        String wrd=sc.nextLine(),temp = "";
-        Deque<Character> deque=new ArrayDeque<>();
-        for(char c:wrd.toCharArray())
-        {
+        String wrd = sc.nextLine();
+
+        // Using Deque
+        String temp = "";
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : wrd.toCharArray()) {
             deque.addLast(c);
         }
-        boolean isPalindrome =true;
-        while(deque.size()>1)
-        {
-            if(deque.removeLast()!=deque.removeFirst()) {
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (deque.removeLast() != deque.removeFirst()) {
                 isPalindrome = false;
-                System.out.println("Not Palindrome");
-                System.exit(0);
+                break;
             }
         }
-        if(isPalindrome)
-            System.out.println("Palindrome");
 
+        if (isPalindrome)
+            System.out.println("Palindrome (using Deque)");
+        else
+            System.out.println("Not Palindrome (using Deque)");
 
+        // Using LinkedList
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : wrd.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindromeList = true;
+
+        while (list.size() > 1) {
+            if (list.removeFirst() != list.removeLast()) {
+                isPalindromeList = false;
+                break;
+            }
+        }
+
+        if (isPalindromeList)
+            System.out.println("Word is Palindrome (using LinkedList)");
+        else
+            System.out.println("Word is not Palindrome (using LinkedList)");
+
+        sc.close();
     }
 }
